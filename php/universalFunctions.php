3715cca5b9a1b27b4f47d8a9781335b5400462b7
ndjndj <teq.ndj@gmail.com>
2020-06-17 00:00:45 +0900

    DB接続作成

--- php/universalFunctions.php
+++ php/universalFunctions.php
@@ -1,9 +1,18 @@
 <?php
 
-    function connection_DBO () {
+    function connectDB ($DB_NAME, $HOST) {
+        $DSN = "mysql:dbname={$DB_NAME};host={$HOST}";
+        $USER_NAME = "root";
+        $PASSWORD = "root";
+        $OPTION = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`");
+        $pdo = new PDO($DSN, $USER_NAME, $PASSWORD, $OPTION);
         
-
-
+        if ($pdo) {
+            return $pdo;
+        } else {
+            echo "<script>window.alert('データベース接続に失敗しました。')</script>";
+            return false;
+        }
 
     }
 
